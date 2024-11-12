db276243de250327ce7a6a2c7102f80d0f94ea4b
Vinicius Gabriel Pereira <80440262+ViniciusGPSOL@users.noreply.github.com>
2024-11-12 07:42:52 -0300

    update Lizard Model

--- app/Models/Lizard.php
+++ app/Models/Lizard.php
@@ -7,8 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 class Lizard extends Model
 {
 
-    protected $table = 'lizards'; //the default way that models to database relationship works, is that the database will have
-    protected $fillable = [
+    // The default way that Models relates to the database is by looking at the Model name,
+    // then getting its plural for example, Lizards will relate to the table lizards. If I have a different table name
+    // I can define it like this:
+    protected $table = "lizards";
+    // Laravel also assumes the primary key is id,
+    // If one wants to change the primary key it must do like this:
+
+    protected $primaryKey = "id";
+
+    //One can also disable timestamps for updated at and created at with this:
+    public $timestamps = false;
+    //One can also hide a field, so that it wont be returned in API response. It is a security measure.
+    //this way, when returning a json from the elements, age, description, species and weight wont be displayed.
+    protected $hidden = ["age", "description", "species", "weight"];
+    //One can then show these elements concatenated with a methtod that will have the name in this format:
+    //getXAttribute, where X is t he name in this var but on camelCase:
+    protected $appends = ["full_description"];
+    //implementation of the method getXAttribute where x is full_description that becomes FullDescriptionAttribute
+
+    public function getFullDescriptionAttribute(): string
+    {
+        return "age: " . (string) $this->age .
+            "species: " . (string) $this->species .
+            "weight: " . (string) $this->weight .
+            "\n" . (string) $this->description; //description in one line
+    }
+
+    protected $fillable = [ //can be mass assigned
         "name",
         "species",
         "age",
@@ -18,7 +44,7 @@ class Lizard extends Model
         "color"
     ];
 
-    protected $casts = [
+    protected $casts = [ //casts fields to given type
         "age" => "integer",
         "weight" => "float",
         "poisonous" => "boolean",
