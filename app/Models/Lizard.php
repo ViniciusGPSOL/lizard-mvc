8b2f0d304e6c04baa9d4329c1a552ce1034130d5
Vinicius Gabriel Pereira <80440262+ViniciusGPSOL@users.noreply.github.com>
2024-11-12 16:50:45 -0300

    added migration to set active lizards as well as a new scope definition

--- app/Models/Lizard.php
+++ app/Models/Lizard.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Lizard extends Model
 {
@@ -13,14 +14,12 @@ class Lizard extends Model
     protected $table = "lizards";
     // Laravel also assumes the primary key is id,
     // If one wants to change the primary key it must do like this:
-
     protected $primaryKey = "id";
-
     //One can also disable timestamps for updated at and created at with this:
     public $timestamps = false;
     //One can also hide a field, so that it wont be returned in API response. It is a security measure.
     //this way, when returning a json from the elements, age, description, species and weight wont be displayed.
-    protected $hidden = ["age", "description", "species", "weight"];
+    protected $hidden = ["age", "description", "species", "weight", "is_active"];
     //One can then show these elements concatenated with a methtod that will have the name in this format:
     //getXAttribute, where X is t he name in this var but on camelCase:
     protected $appends = ["full_description"];
@@ -34,14 +33,20 @@ class Lizard extends Model
             "\n" . (string) $this->description; //description in one line
     }
 
-    protected $fillable = [ //can be mass assigned
+    //scope definition of a Lizard that is active, must use Lizard::active()->get()
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->where("is_active", true);
+    }
+    protected $fillable = [ //can be mass assigned to not make it able to mass assign one should use $guarded
         "name",
         "species",
         "age",
         "weight",
         "description",
         "poisonous",
-        "color"
+        "color",
+        "is_active"
     ];
 
     protected $casts = [ //casts fields to given type
