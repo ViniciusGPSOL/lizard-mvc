d05a191004691e84a079e7a15def31b0d77d1906
Vinicius Gabriel Pereira <80440262+ViniciusGPSOL@users.noreply.github.com>
2024-11-12 07:36:20 -0300

    update Lizards Model

--- app/Models/Lizard.php
+++ app/Models/Lizard.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lizard extends Model
 {
+
+    protected $table = 'lizards'; //the default way that models to database relationship works, is that the database will have
     protected $fillable = [
         "name",
         "species",
